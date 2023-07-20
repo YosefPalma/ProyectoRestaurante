69082232d284ee61b9df7bcd1670908ce9fc6f3a
Julio Chan <133795465+Jchhm24@users.noreply.github.com>
2023-07-20 11:57:27 -0600

    VentanadaModal_Crear_Cuenta

---
+++ js/log_in_modales.js
@@ -0,0 +1,14 @@
+/* Ventana modal para crear cuentas */
+
+// Abrir ventana modal
+let modal_open_create_account=()=>{
+    modal_sing_up.showModal();
+}
+
+// Cerrar ventana modal
+let modal_close_create_account=()=>{
+    modal_sing_up.close();
+}
+// Crear función de aviso de que se creo una cuenta
+
+/* Ventana modal para recordar contraseña */

fc5343b8754691433e8fa4538e1e90a44aaede97
Julio Chan <133795465+Jchhm24@users.noreply.github.com>
2023-07-27 00:09:19 -0600

    crud_html_edit_account

--- js/perfil.js
+++ js/perfil.js
@@ -1,3 +1,14 @@
 // Parte de Julio Cesar Chan Manrique
 var valor=localStorage.getItem("usernames");
 document.write(valor);
+
+const button = document.getElementById("edit_dates");
+const form = document.getElementById("editor");
+
+button.addEventListener("click", () => {
+  if (form.classList.contains("hidden")) {
+    form.classList.remove("hidden");
+  } else {
+    form.classList.add("hidden");
+  }
+});
